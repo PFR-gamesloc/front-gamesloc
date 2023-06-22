ce86e1226dbbf0008bf8376462e38d168bc2a61a
SGdevWeb <samuel.gustin.dev@gmail.com>
2023-06-22 22:37:00 +0200

    Add input component

---
+++ src/app/shared/components/input/input.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+})
+export class InputComponent {
+  @Input() forInput: string | undefined;
+  @Input() textLabel: string | undefined;
+  @Input() typeInput: string | undefined;
+  @Input() placeholderInput: string | undefined;
+  @Input() idInput: string | undefined;
+}
