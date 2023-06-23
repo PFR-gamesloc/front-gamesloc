264fb8f3f832e6be80fa741e0c1f6c0b79e97123
Gaetan Mgld <m.gaetanpro@gmail.com>
2023-06-23 12:25:38 +0200

    Create card article and Home page test for test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 import { BtnComponentComponent } from './shared/components/btn/btn.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CardItemComponent } from './shared/components/card-item/card-item.component';
+import { HomeTestComponent } from './shared/components/home-test/home-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     BtnComponentComponent,
+    CardItemComponent,
+    HomeTestComponent,
   ],
   imports: [
     BrowserModule,
