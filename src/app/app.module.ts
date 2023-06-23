7d9396fb0a79124887c24f02c57e288b7fb97272
Gaetan Mgld <m.gaetanpro@gmail.com>
2023-06-23 16:11:06 +0200

    Component carousel and task no finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BtnComponent } from "./shared/components/btn/btn.component";
 import { ProductPageComponent } from './modules/users/pages/product-page/product-page.component';
 import { CardItemComponent } from './shared/components/card-item/card-item.component';
 import { HomeTestComponent } from './shared/components/home-test/home-test.component';
+import { CarrouselComponent } from './shared/components/carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { HomeTestComponent } from './shared/components/home-test/home-test.compo
     ProductPageComponent,
     CardItemComponent,
     HomeTestComponent,
+    CarrouselComponent,
   ],
   imports: [
     BrowserModule,
