cd2d46e6e9eecc1bdf3e5506a5850ea7c3b69c00
Demeester Bryan <demeesterbryan0@gmail.com>
2023-08-20 12:46:58 +0200

    fix favorite-item linksÃ

--- src/app/modules/users/components/favorite-item-accordion/favorite-item-accordion.component.ts
+++ src/app/modules/users/components/favorite-item-accordion/favorite-item-accordion.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {CustomerLike} from 'src/app/shared/entities/customerLike';
 import {GetService} from "../../../../core/http/get.service";
+import {FileUploadService} from "../../../admin/services/file-upload.service";
+import {GameService} from "../../../../core/http/games.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-favorite-item-accordion',
@@ -9,15 +12,30 @@ import {GetService} from "../../../../core/http/get.service";
 })
 export class FavoriteItemAccordionComponent {
   favoriteGames: CustomerLike[];
-
+  image:string ='';
   constructor(
-    private getService: GetService) {
+    private getService: GetService,
+    private fileService: FileUploadService,
+    private gameService:GameService,
+    private router:Router) {
     this.favoriteGames = [];
   }
 
   ngOnInit(): void {
     this.getService.getData<CustomerLike[]>("customer/me/favs").subscribe({
-      next: (res: CustomerLike[]) => this.favoriteGames = res
+      next: (customerLikes: CustomerLike[]) => {
+        this.favoriteGames = customerLikes;
+        for (const customerLike of customerLikes) {
+          this.fileService.getFile(customerLike.image).subscribe({
+            next:res=> this.image = URL.createObjectURL(res)
+          })
+        }
+      }
     });
+  }
+
+  navigateToGameDetails(gameId: number, gameName: string) {
+    this.gameService.setSelectedGameId(gameId);
+    this.router.navigate(['game', gameName]);
   }
 }
